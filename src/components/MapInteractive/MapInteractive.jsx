fff94d343f720fe0200b47122719a13cbec9e831
”Ismail” <ibeno95@mail.ru>
2023-04-05 18:05:34 +0300

    Сделал адаптив для карточек и футера, добавил кнопку открытия/закрытия карты

--- src/components/MapInteractive/MapInteractive.jsx
+++ src/components/MapInteractive/MapInteractive.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./mapInter.module.css";
 
 const MapInteractive = () => {
+  const [mapOpen, setMapOpen] = useState(false);
+
+  const toggleMap = () => {
+    setMapOpen(!mapOpen)
+  }
   return (
     <div className={styles.mapInter}>
-      <iframe
+      <button onClick={toggleMap} className={styles.toggleMapBtn}>{mapOpen ? "Скрыть карту" : "Открыть карту"}</button>
+      <iframe className={styles.mapFrame}
+        title="dinersMap"
         src="https://yandex.ru/map-widget/v1/?um=constructor%3A4d70bc60c03110cd1d592db84bb7f29008d789dc484e21d6556492d615571bcc&amp;source=constructor"
-        width="86%"
-        height="450"
+        width="100%"
+        height={mapOpen ? "450px" : 0}
         frameBorder="0"
       ></iframe>
     </div>
